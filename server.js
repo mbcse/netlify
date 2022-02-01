0c130b7c3b96d501adc2dcb08f8e36f9c297780b
mbcse <mbcse50@gmail.com>
2022-02-02 00:44:25 +0530

    fix

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express'); 
 var app = express();
 app.use(express.static(__dirname + '/src'));
+const { ThirdwebSDK } = require("@3rdweb/sdk");
+
+const module = sdk.getTokenModule("0x9b9Bc3EEFa9165CD93eE2f438F70933aC03EDaB7");
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log('\x1b[32m'," WEBAPP STARTED AT http://localhost:"+(process.env.PORT||3000));

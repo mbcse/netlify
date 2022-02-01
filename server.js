50ee362b45e16df64aaf4ae48e5657e1eb1661b5
mbcse <mbcse50@gmail.com>
2022-02-02 00:49:42 +0530

    fix

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ var app = express();
 app.use(express.static(__dirname + '/src'));
 const { ThirdwebSDK } = require("@3rdweb/sdk");
 
-const module = sdk.getTokenModule("0x9b9Bc3EEFa9165CD93eE2f438F70933aC03EDaB7");
+const tokenmodule = sdk.getTokenModule("0x9b9Bc3EEFa9165CD93eE2f438F70933aC03EDaB7");
+const votemodule = sdk.getVoteModule("0x7A201eC9815390d3956bBe8c2843CA62bB5D24f6");
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log('\x1b[32m'," WEBAPP STARTED AT http://localhost:"+(process.env.PORT||3000));
 });
+
+module.exports = {
+    tokenmodule,
+    votemodule
+}
